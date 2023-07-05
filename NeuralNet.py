5beb31540b8501b8a2ae9aade11ff5e208baee34
Jeremy Varghese <jeremy.varghese11@gmail.com>
2023-07-05 14:57:07 +0530

    This tensorflow shit is annoying to sort out

--- NeuralNet.py
+++ NeuralNet.py
@@ -8,3 +8,16 @@ Created on Tue Jul  4 16:01:22 2023
 '''
 Implementing Neural Net
 '''
+from tensorflow.keras import models, layers, utils, backend as K
+import matplotlib.pyplot as plt
+import shap
+
+model = models.Sequential(name = "Perceptron", layers = [
+    layers.Dense(
+        name="dense",
+        input_dim = 19,
+        units = 1,
+        activation = "linear"
+        )
+    ])
+model.summary()
