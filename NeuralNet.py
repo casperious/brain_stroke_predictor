46d526dfffbef057edda5239afec7bef9ae5c1f5
Jeremy Varghese <jeremy.varghese11@gmail.com>
2023-07-06 15:07:07 +0530

    ANN WORKS!!!!

--- NeuralNet.py
+++ NeuralNet.py
@@ -9,8 +9,8 @@ Created on Tue Jul  4 16:01:22 2023
 Implementing Neural Net
 '''
 from tensorflow.keras import models, layers, utils, backend as K
-import matplotlib.pyplot as plt
-import shap
+#import matplotlib.pyplot as plt
+#import shap
 
 model = models.Sequential(name = "Perceptron", layers = [
     layers.Dense(
