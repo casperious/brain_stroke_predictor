41560fe75c1ccfa8fb53c3a5b5d0cbf46a8f9e39
Jeremy Varghese <jeremy.varghese11@gmail.com>
2023-07-18 15:56:59 +0530

    prediction is still not 100% accurate for current data passed in either

--- Input.py
+++ Input.py
@@ -5,8 +5,6 @@ Created on Fri Jun 23 15:56:23 2023
 @author: jeremy
 """
 
-#import SVM as svm
-#import RandomForest as rf
 import classify_new as cn
 import numpy as nm
 import matplotlib.pyplot as mtp
@@ -15,19 +13,12 @@ import tkinter
 from tkinter import *
 from tkinter import filedialog
 
-#data_set_new = pd.read_csv('new_patient_data.csv')
 
 def openFile():
     filePath = filedialog.askopenfilename()
-    #print(filePath)
-    #data_set_new_svm = pd.read_csv(filePath)
     data_set_new_rf = pd.read_csv(filePath)
-    #print(data_set_new_rf.type)
-    #svm.ClassifyNew(data_set_new_svm)
-    #rf.ClassifyNew(data_set_new_rf)
     cn.ClassifyNew(data_set_new_rf)
 
-#accept new csv file to read and predict
 window = Tk()
 button = Button(text = "Open file", command = openFile)
 button.pack()
